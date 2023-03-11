869e0ae317535284e89ccda3c4112a3ea542f3e7
nehagarg0209 <35847498+nehagarg0209@users.noreply.github.com>
2023-03-11 20:53:36 +0530

    split api into variables

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ streamlit.dataframe(fruits_to_show)
  
   #======================= request api ================================ #
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon");
-streamlit.text(fruityvice_response.json());
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi");
+# streamlit.text(fruityvice_response.json());
 
 # take the json version of response and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
