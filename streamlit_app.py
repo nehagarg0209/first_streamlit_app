93536e9e95c3235195008ca03f0d82600581d562
nehagarg0209 <35847498+nehagarg0209@users.noreply.github.com>
2023-03-12 00:07:33 +0530

    ui enhancement

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,6 @@ streamlit.text(my_data_row)
 
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST;")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
 
